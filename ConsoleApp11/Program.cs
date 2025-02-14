83db62bd93e0cd5e008ac83ec23e8c8ba92088da
MEGA <MEGA@gmail.com>
2025-02-14 18:14:03 +0200

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    
+    static int Reverse(int n, int reversed = 0)
+    {
+       
+        if (n == 0)
+            return reversed;
+
+        
+        int lastDigit = n % 10;
+        reversed = reversed * 10 + lastDigit;
+
+        
+        return Reverse(n / 10, reversed);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Въведете число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int reversedNum = Reverse(num);
+        Console.WriteLine($"Обръщането на числото {num} е {reversedNum}.");
+    }
+}
+
